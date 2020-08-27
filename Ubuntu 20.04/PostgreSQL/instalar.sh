079fb7d4a12e34d73985a830000ba081b5a84e6f
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-08-27 13:40:09 -0300

    Adicionando o script de instalação do PostgreSQL

---
+++ Ubuntu 20.04/PostgreSQL/instalar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Adicionando os repositorios do PostgreSQL e do pgAdmin
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+
+#Adicionando a chave do PostgreSQL e do pgAdmin
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+
+#Atualizando os repositorios apt
+sudo apt update
+
+#Instalando o PostgreSQL e do pgAdmin
+sudo apt install postgresql pgadmin4 pgadmin4-desktop pgadmin4-web -y;
+
+#Configurando o servidor web do pgAdmin
+sudo /usr/pgadmin4/bin/setup-web.sh
